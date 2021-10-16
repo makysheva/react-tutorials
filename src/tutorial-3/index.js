a66c94f9708cd1a0799896976c0ef3e253813302
makysheva <makysheva.aliya@gmail.com>
2021-10-16 08:05:12 +0600

    решение с правильной структурой, логикой. от ментора

--- src/tutorial-3/index.js
+++ src/tutorial-3/index.js
@@ -3,31 +3,70 @@ const valueTask = document.getElementById('valueTask');
 const addBtn = document.getElementById('addBtn');
 const ul = document.createElement('ul');
 let tasks = [];
+const ulElem = document.querySelector('ul');
+const inputElem = document.querySelector('input');
+let listeners = [];
+
+const removeTask = (index) => {
+  const [func, removeBtn] = listeners[index];
+
+  removeBtn.removeEventListener('click', func);
+
+  tasks = tasks.filter((_, i) => index !== i);
+  renderUl();
+};
+
+const renderUl = () => {
+  ulElem.innerHTML = '';
+  tasks.forEach((str, i) => {
+    const li = document.createElement('li');
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'X';
+
+    const removeFunc = () => removeTask(i);
+    listeners.push([removeFunc, removeBtn]);
+
+    li.innerText = str;
+    li.appendChild(removeBtn);
+    removeBtn.addEventListener('click', removeFunc);
+    // removeBtn.onclick = () => removeTask(i)
+    ulElem.appendChild(li);
+  });
+};
 
 const handleClickAdd = () => {
-  return tasks.push(valueTask.value);
+  const { value } = inputElem;
+  tasks.push(value);
+  inputElem.value = '';
+  renderUl();
 };
 
-const render = () => {
-  const li = document.createElement('li');
-  const closeBtn = document.createElement('button');
-  container.before(ul);
-  ul.appendChild(li);
+document.querySelector('#addBtn').onclick = handleClickAdd;
 
-  if (handleClickAdd()) {
-    li.appendChild(document.createTextNode(`${valueTask.value}  `));
-    valueTask.value = '';
-  }
+// const handleClickAdd = () => {
+//   return tasks.push(valueTask.value);
+// };
 
-  li.appendChild(closeBtn);
-  closeBtn.appendChild(document.createTextNode('X'));
-  closeBtn.className = 'closeBtn';
+// const render = () => {
+//   const li = document.createElement('li');
+//   const closeBtn = document.createElement('button');
+//   container.before(ul);
+//   ul.appendChild(li);
 
-  const handleClickRemove = () => {
-    li.remove();
-  };
+//   if (handleClickAdd()) {
+//     li.appendChild(document.createTextNode(`${valueTask.value}  `));
+//     valueTask.value = '';
+//   }
 
-  closeBtn.addEventListener('click', handleClickRemove);
-};
+//   li.appendChild(closeBtn);
+//   closeBtn.appendChild(document.createTextNode('X'));
+//   closeBtn.className = 'closeBtn';
 
-addBtn.addEventListener('click', render);
+//   const handleClickRemove = () => {
+//     li.remove();
+//   };
+
+//   closeBtn.addEventListener('click', handleClickRemove);
+// };
+
+// addBtn.addEventListener('click', render);
