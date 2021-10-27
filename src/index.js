113903da2b56da9f3223def91cf7e2b8b6f5cb38
makysheva <makysheva.aliya@gmail.com>
2021-10-27 09:16:11 +0600

    задача №6. Страницы сайта, навигация

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 // import App from './tutorial-1/App';
 // import App from './tutorial-2/App';
 // import App from './tutorial-4/App';
-import App from './tutorial-5/App';
+// import App from './tutorial-5/App';
+import App from './tutorial-6/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
