4957de8058220a72bb6f0e263791e3e9868e76e6
makysheva <makysheva.aliya@gmail.com>
2021-10-30 15:50:22 +0600

    задача №8. Получение данных с сервера с помощью async/await

---
+++ src/tutorial-8/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const App = () => {
+  const [users, setUsers] = React.useState([]);
+
+  async function getUsers() {
+    let response = await fetch('https://617d0c8a1eadc5001713638e.mockapi.io/users');
+    let users = await response.json();
+    let result = await setUsers(users);
+
+    return result;
+  }
+
+  return (
+    <div>
+      <ul>
+        {users.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+      <button onClick={getUsers}>Список пользователей</button>
+    </div>
+  );
+};
+
+export default App;
